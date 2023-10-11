9ce94ae298f4360ee4ffe85576b46458bfa1d18d
Alumno <Alumno@LAB1504-18>
2023-10-10 21:39:41 -0500

    Agregar archivos de proyecto.

---
+++ Business/BProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+
+namespace Business
+{
+    public class BProduct
+    {
+        public List<Product> GetByName(string Name)
+        {
+
+            DProduct data = new DProduct();
+            var products = data.Get();
+
+            var result= products.Where(x=> x.Name.Contains(Name)).ToList();
+            //lambda
+            //flecha
+            
+
+            return result;
+        }
+    }
+}
